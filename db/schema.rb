b75df0220c6895ce5868d3fa8df5a957320b0285
Shawn Kearney <shawnpk29@me.com>
2026-01-13 09:49:13 -0700

    Generate the movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_13_164328) do
+  create_table "movies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "rating"
+    t.string "title"
+    t.decimal "total_gross"
+    t.datetime "updated_at", null: false
+  end
 end
