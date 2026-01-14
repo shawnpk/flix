b2c9341df4166265b8545b6cef797dd0e9fba37d
Shawn Kearney <shawnpk29@me.com>
2026-01-13 21:12:38 -0700

    Add edit route, controller action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "movies", to: "movies#index"
   get "movies/:id", to: "movies#show", as: :movie
+  get "movies/:id/edit", to: "movies#edit", as: :edit_movie
 
   root "movies#index"
 end
