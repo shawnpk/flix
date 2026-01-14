6e110ef36be36320ed77d495d507ad0cf9c617b8
Shawn Kearney <shawnpk29@me.com>
2026-01-14 10:50:22 -0700

    Add custom query to only show upcoming movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  scope :released, -> { where("released_on > ?", Time.current).order(released_on: :desc) }
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
