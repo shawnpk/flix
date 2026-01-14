594a16c50f30b1d543793d63e534a3066be96180
Shawn Kearney <shawnpk29@me.com>
2026-01-14 09:33:46 -0700

    Add ability to create movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get "movies", to: "movies#index"
-  get "movies/:id", to: "movies#show", as: :movie
-  get "movies/:id/edit", to: "movies#edit", as: :edit_movie
-  patch "movies/:id", to: "movies#update"
+  resources :movies
 
   root "movies#index"
 end
