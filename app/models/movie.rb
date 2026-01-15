7f139f6e220cf4f36e7105df63d7990452a7b4c4
Shawn Kearney <shawnpk29@me.com>
2026-01-14 17:04:14 -0700

    Add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,15 @@
 class Movie < ApplicationRecord
+  RATINGS = %w[G PG PG-13 R NC-17]
+
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, inclusion: { in: RATINGS }
+  validates :image_file_name, format: {
+    with: /\A\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
+
   scope :released, -> { where("released_on > ?", Time.current).order(released_on: :desc) }
 
   def flop?
