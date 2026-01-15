3e0acbae7d4d8b7a5c09054406fe45b0a3185b87
Shawn Kearney <shawnpk29@me.com>
2026-01-15 13:51:07 -0700

    Show average stars on index and show views

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,6 +15,12 @@ class Movie < ApplicationRecord
   scope :released, -> { where("released_on > ?", Time.current).order(released_on: :desc) }
 
   def flop?
-    total_gross.blank? || total_gross < 225_000_000
+    unless reviews.count >= 50 && average_stars >= 4
+      (total_gross.blank? || total_gross < 225_000_000)
+    end
+  end
+
+  def average_stars
+    reviews.average(:stars) || 0.0
   end
 end
