b39a970f8e7fe2b240ce5b29437ac434472ca030
Shawn Kearney <shawnpk29@me.com>
2026-01-14 18:52:05 -0700

    Show validation errors in the UI

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,17 +18,21 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
 
-    @movie.save
-
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_content
+    end
   end
 
   def update
     @movie = Movie.find(params[:id])
 
-    @movie.update(movie_params)
-
-    redirect_to @movie
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit, status: :unprocessable_content
+    end
   end
 
   def destroy
