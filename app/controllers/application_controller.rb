0334be144d4bd69dc3c335509136deca8393e3a0
Shawn Kearney <shawnpk29@me.com>
2026-01-16 07:23:18 -0700

    Add require signin method to gatekeep user resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def require_signin
+    redirect_to new_session_path, alert: "Please sign in first" unless current_user
+  end
 end
