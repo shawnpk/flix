9311b0b920e87a402fdc9be803eb9081176bd7f5
Shawn Kearney <shawnpk29@me.com>
2026-01-15 17:02:32 -0700

    Add ability for users to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: "Thanks for signing up"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [ :name, :email, :password, :password_confirmation ])
+  end
 end
