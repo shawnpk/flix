69ff9d35c5f924952a2657adb512b1b443d9be56
Shawn Kearney <shawnpk29@me.com>
2026-01-16 13:39:23 -0700

    Add through associations for movie and user

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :favorites, dependent: :destroy
+  has_many :fans, through: :favorites, source: :user
   has_many :reviews, dependent: :destroy
 
   RATINGS = %w[G PG PG-13 R NC-17]
