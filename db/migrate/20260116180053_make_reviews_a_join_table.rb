7222ac28e34cca39e310d07dc48b665fdeb83483
Shawn Kearney <shawnpk29@me.com>
2026-01-16 11:07:31 -0700

    Chagne reviews to a many to many association

---
+++ db/migrate/20260116180053_make_reviews_a_join_table.rb
@@ -0,0 +1,8 @@
+class MakeReviewsAJoinTable < ActiveRecord::Migration[8.1]
+  def change
+    remove_column :reviews, :name, :string
+    add_column :reviews, :user_id, :integer
+
+    Review.destroy_all
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_16_164028) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_16_180053) do
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -28,9 +28,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_16_164028) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.integer "movie_id", null: false
-    t.string "name"
     t.integer "stars"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
