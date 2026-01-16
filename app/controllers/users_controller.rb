41ebfb6963d21ca84a5ff7e5df6fd543831bfdbb
Shawn Kearney <shawnpk29@me.com>
2026-01-15 18:11:14 -0700

    Add ability to edit and destroy account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -21,7 +25,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated"
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @user.destroy
+
+    redirect_to movies_path, alert: "Account successfully deleted", status: :see_other
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.expect(user: [ :name, :email, :password, :password_confirmation ])
